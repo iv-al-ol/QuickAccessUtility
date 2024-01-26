d154afa38b77915c296f25561941e38c1c62d069
iv-al-ol ekra <mr.upon@yandex.ru>
2024-01-26 13:16:00 +0300

    Добавление сохранения и загрузки в json файл

--- model.py
+++ model.py
@@ -1,9 +1,30 @@
+import os
+import json
+
+
 class SettingsModel:
     def __init__(self):
         self.startup_enabled = False
         self.startup_minimized = False
         self.tray_enabled = False
+        self.set_settings_file()
+        self.load_settings()
+
+    def set_settings_file(self):
+        """Опреледяет папку и файл настроек."""
+        self.settings_folder = os.path.join(os.path.expanduser("~"),
+                                             "Documents", 
+                                             "QuickAccessUtility")
+        self.settings_file = os.path.join(self.settings_folder, "settings.json")
 
     def save_settings(self):
-        # Реализация сохранения настроек
+        """Сохраняет настройки."""
+        settings = {
+            "startup_enabled": self.startup_enabled,
+            "startup_minimized": self.startup_minimized,
+            "tray_enabled": self.tray_enabled
+        }
+
+    def load_settings(self):
+        """Загружает настройки."""
         pass
