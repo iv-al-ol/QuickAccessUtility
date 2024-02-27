2fb9511c443b0f58a4ff1ca38ec2f6f4a6ef60db
iv-al-ol ekra <mr.upon@yandex.ru>
2024-02-27 13:14:31 +0300

    Скорректировал сворачивание в трей

--- view.py
+++ view.py
@@ -9,7 +9,7 @@ class SettingsView(QMainWindow):
         super().__init__()
 
         self.setWindowTitle("Quick Access Utility")
-        self.setWindowIcon(QIcon('images\\app_icon.ico'))
+        self.setWindowIcon(QIcon("images\\app_icon.ico"))
         self.setGeometry(100, 100, 400, 200)
 
         self.startup_checkbox = QCheckBox("Запускать приложение при запуске системы", self)
@@ -24,45 +24,43 @@ class SettingsView(QMainWindow):
         self.save_button = QPushButton("Сохранить настройки", self)
         self.save_button.setGeometry(10, 130, 150, 30)
 
-        # Создание трея
-
-
         # Иконка в трее
         self.tray_icon = QSystemTrayIcon(self)
-        self.tray_icon.setIcon(self.style().standardIcon(QStyle.SP_ComputerIcon))
-        self.tray_icon.setToolTip('Quick Access Utility')
+        self.tray_icon.setIcon(QIcon("images\\app_icon.ico"))
+        self.tray_icon.setToolTip("Quick Access Utility")
 
-        show_action = QAction("Show", self)
-        quit_action = QAction("Exit", self)
-        hide_action = QAction("Hide", self)
+        # События трея
+        show_action = QAction("Показать", self)
+        hide_action = QAction("Скрыть", self)
+        quit_action = QAction("Выйти", self)
+
         show_action.triggered.connect(self.show)
         hide_action.triggered.connect(self.hide)
-        #quit_action.triggered.connect(self.quit)
+        quit_action.triggered.connect(exit)
+
+        # Инициализация пунктов меню трея
         tray_menu = QMenu()
         tray_menu.addAction(show_action)
         tray_menu.addAction(hide_action)
         tray_menu.addAction(quit_action)
+
         self.tray_icon.setContextMenu(tray_menu)
         self.tray_icon.show()
 
-        '''
-        # Контекстное меню трея
-        self.tray_menu = QMenu()
-        self.tray_menu.addAction("Открыть", self.show)
-        self.tray_menu.addAction("Закрыть приложение", self.close)
-
-        self.tray_icon.setContextMenu(self.tray_menu)
-        '''
-
     def closeEvent(self, event):
-        #if self.check_box.isChecked():
+        """Переназначение функции закрытия окна для возможности сворачивания."""
+        #if self.check_box.isChecked(): Добавить настройку сворачивания приложения в трей
         event.ignore()
         self.hide()
-        self.tray_icon.showMessage(
-            "Tray Program",
-            "Application was minimized to Tray",
-            QSystemTrayIcon.Information,
-            2000)
+        # Добавить возможность отключения уведомления о сворачивании приложения
+        '''
+        if model.settings.tray_notification:
+            self.tray_icon.showMessage(
+                "Quick Access Utility",
+                "Приложение свернуто в трей",
+                QSystemTrayIcon.Information,
+                1000)
+        '''
 
     def show_message(self, message):
         # Реализация вывода сообщений пользователю
